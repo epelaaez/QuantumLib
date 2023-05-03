d6f939cb9506cc6f708672dfcdad5a0f10d1122c
Emilio Pelaez <epelaaez@gmail.com>
2023-05-03 12:25:35 -0500

    add well typedness of path sum

--- PathSums.v
+++ PathSums.v
@@ -7,7 +7,7 @@ Import ListNotations.
 (*
 TODO: 
 - [ ] Figure out better notation to distinguish from unitary semantics.
-- [ ] Define path-sum inductive semantics
+- [x] Define path-sum inductive semantics
 - [ ] Define function equivalent to uc_eval that puts the information in the sum/exponent form
 - [ ] Prove compose (both in parallel and in sequence) lemmas using path-sum semantics
 - [ ] Define a standard basis in path-sum (e.g., CNOT, SWAP, ID, Ph(n), Rz(n), H)
@@ -86,6 +86,21 @@ Definition P_Ph (n : nat) (x : bv 1) := psum_1 (Cexp ((2 * PI) / (2 ^ n))) x.
 Definition P_Rz (n : nat) (x : bv 1) := psum_1 (Cexp (((-1 ^ (1 - x[0]))) * (2 * PI) / (2 ^ n))) x.
 *)
 
-(* Define well-formedness *)
+Local Close Scope C_scope.
+Local Close Scope R_scope.
+
+(* Define well-typedness *)
+Inductive pscom_well_typed {P width} : pscom P width -> Prop :=
+    | WT_pseq : forall p1 p2, pscom_well_typed p1 -> pscom_well_typed p2 -> pscom_well_typed (p1, p2)
+    | WT_pseq1 : forall p n, n < width -> pscom_well_typed (papp1 p n)
+    | WT_pseq2 : forall p m n, n < width -> m < width -> n <> m -> pscom_well_typed (papp2 p n m).
+
+(* Well-typed path sums have width > 0 *)
+Lemma pscom_well_typed_implies_width_nonzero : forall {P width} (p : pscom P width),
+    pscom_well_typed p -> width > 0.
+Proof.
+    intros P width p WT.
+    induction WT; try apply (Nat.lt_lt_0 n); assumption.
+Qed.
 
 (* Define pseq and prove semantics of Y, Z *)
